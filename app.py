9920b211f28a003ea4cea46019762a9f5c3b7160
Spiderman-cell <Sparshan144@gmail.com>
2025-03-11 10:32:14 -0600

    Visaulization in streamlit

--- app.py
+++ app.py
@@ -22,7 +22,60 @@ col1, col2 = st.columns(2)
 with col1:
     sentiment_counts = df['sentiment_label'].value_counts().reset_index()
     sentiment_counts.columns = ['sentiment_label','count']
-    fig_pie =px.pie
+    fig_pie =px.pie(
+        sentiment_counts,
+        values='Count',
+        names='Sentiment',
+        title='Sentiment Distribution',
+        color='Sentiment_label',
+        color_discrete_map={'positive':'green','neutral':'gray','negative':'red'})
+    st.plotly_chart(fig_pie,use_container_width=True)
+
+with col2:
+    fig_bar=px.bar(
+        sentiment_counts,
+        x='Sentiment',
+        y='Count',
+        title='Sentiment Counts',
+        color='sentiment_label',
+        color_discrete_map={'positive':'green','neutral':'gray','negative':'red'} 
+    )
+    st.plotly_chart(fig_bar,use_container_width=True)
+
+st.header('sentiment scores visualization')
+col3, col4= st.columns(2)
+
+with col3:
+    fig_scatter=px.scatter(
+        df,
+        x='positive',
+        y='negative',
+        color='setiment_label',
+        hover_data=['cleaned_caption'] if 'cleaned_caption' in df.columns else None,
+        title='Positive Vs Negative Sentiment Scores',
+        color_discrete_map={'positive':'green','neutral':'gray','negative':'red'}
+    )
+    st.plotly_chart(fig_scatter,use_container_width=True)
+
+with col4:
+    fig_3d =px.scatter_3d(
+        df,
+        x='positive',
+        y='negative',
+        z='neutral',
+        color='sentiment_label',
+        hover_data=['text'] if 'text' in df.columns else None,
+        title='Positive vs Negative Sentiment Scores',
+        color_discrete_map={'positive':'green','neutral':'gray','negative':'red'}
+    )
+    fig_3d.update_layout(scene=dict(
+        xaxis_title='Positive',
+        yaxis_title='Negative',
+        zaxis_title='Neutral'
+    ))
+    st.plotly_chart(fig_3d, use_container_width=True)
+
+
 
 
          
