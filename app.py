2d4c9092cae8ec9b15221de55e8275a592c8dc54
Spiderman-cell <Sparshan144@gmail.com>
2025-03-12 09:11:57 -0600

    b

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(BASE_DIR, "Nltk_Vader", "Sentiment_results.csv")
 
 df = pd.read_csv(csv_path)
+st.write(df.head())
 
 # Count the number of occurrences for each sentiment
 df['sentiment_label'] = df['sentiment_label'].str.lower().str.strip()
 sentiment_counts = df['sentiment_label'].value_counts().reset_index()
 sentiment_counts.columns = ['sentiment_label', 'count']
+st.write(sentiment_counts)
 
 
 col1, col2 = st.columns(2)
@@ -50,34 +52,34 @@ with col2:
 
 st.header('Sentiment Scores Visualization')
 
-col3, col4 = st.columns(2)
+# col3, col4 = st.columns(2)
 
-with col3:
-    fig_scatter = px.scatter(
-        df,
-        x='positive',
-        y='negative',
-        color='sentiment_label',  # Fixed issue: Corrected column name
-        hover_data=['cleaned_caption'] if 'cleaned_caption' in df.columns else None,
-        title='Positive Vs Negative Sentiment Scores',
-        color_discrete_map={'positive': 'green', 'neutral': 'gray', 'negative': 'red'}
-    )
-    st.plotly_chart(fig_scatter, use_container_width=True)
+# with col3:
+#     fig_scatter = px.scatter(
+#         df,
+#         x='positive',
+#         y='negative',
+#         color='sentiment_label',  # Fixed issue: Corrected column name
+#         hover_data=['cleaned_caption'] if 'cleaned_caption' in df.columns else None,
+#         title='Positive Vs Negative Sentiment Scores',
+#         color_discrete_map={'positive': 'green', 'neutral': 'gray', 'negative': 'red'}
+#     )
+#     st.plotly_chart(fig_scatter, use_container_width=True)
 
-with col4:
-    fig_3d = px.scatter_3d(
-        df,
-        x='positive',
-        y='negative',
-        z='neutral',
-        color='sentiment_label',
-        hover_data=['text'] if 'text' in df.columns else None,
-        title='Positive vs Negative Sentiment Scores',
-        color_discrete_map={'positive': 'green', 'neutral': 'gray', 'negative': 'red'}
-    )
-    fig_3d.update_layout(scene=dict(
-        xaxis_title='Positive',
-        yaxis_title='Negative',
-        zaxis_title='Neutral'
-    ))
-    st.plotly_chart(fig_3d, use_container_width=True)
+# with col4:
+#     fig_3d = px.scatter_3d(
+#         df,
+#         x='positive',
+#         y='negative',
+#         z='neutral',
+#         color='sentiment_label',
+#         hover_data=['text'] if 'text' in df.columns else None,
+#         title='Positive vs Negative Sentiment Scores',
+#         color_discrete_map={'positive': 'green', 'neutral': 'gray', 'negative': 'red'}
+#     )
+#     fig_3d.update_layout(scene=dict(
+#         xaxis_title='Positive',
+#         yaxis_title='Negative',
+#         zaxis_title='Neutral'
+#     ))
+#     st.plotly_chart(fig_3d, use_container_width=True)
