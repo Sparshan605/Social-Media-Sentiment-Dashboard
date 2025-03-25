d1cb50b6d411bc504e5708f466b7657085929ba1
Spiderman-cell <Sparshan144@gmail.com>
2025-03-24 22:15:23 -0600

    error in model

--- prediction.py
+++ prediction.py
@@ -3,7 +3,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def predict(text_data):
     model = joblib.load("output_models/logistic_regression_model.sav")
-    max_features = min(5000, max(100, len(set(' '.join(text_data).split()))))
-    vectorizer = TfidfVectorizer(max_features=max_features)
+    vectorizer = TfidfVectorizer(max_features=27111)
     X_transformed = vectorizer.fit_transform(text_data)
     return model.predict(X_transformed)
